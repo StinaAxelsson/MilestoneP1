4febe50610df627e8e8bd0a37202bb394a1421d3
StinaAxelsson <stina91@live.se>
2021-07-23 21:18:50 +0000

    testar lite canvas. Behöver fixa spaceship eller någpt

--- script.js
+++ script.js
@@ -1,61 +1,29 @@
-let gameArea = document.getElementById('canvas')
-let score = document.getElementById('score-div')
-let startButton = document.getElementById('start')
-let spaceShip = document.getElementById('spaceship1')
+const canvas = document.getElementById("gameArea");
+const ctx = canvas.getContext("2d");
 
-startButton.addEventListener("click", start);
+let player = getElementById("spaceship")
 
-let player ={score:0, speed:2};
-let keys= {ArrowUp:false, ArrowDown:false, ArrowRight:false, ArrowLeft:false};
-
-document.addEventListener('keydown', keyDown);
-document.addEventListener('keyup', keyUp);
-
-function keyDown(e){
-    e.preventDefault();
-    keys[e.key]=true;
+//Game Loop
+function drawGame(){
+    requestAnimationFrame(drawGame);
+    clearScreen();
+    player();
+    
+}
+function player(){
+    
 }
 
-function keyUp(e){
-    e.preventDefault();
-    keys[e.key]=false;
+function clearScreen(){
+    ctx.fillStyle = "pink";
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
 }
 
-function gamePlay(){
+document.body.addEventListener("keydown", keyDown);
+document.body.addEventListener("keyup", keyUp);
 
-    
-    let road= gameArea.getBoundingClientRect();
-    
-    if(player.start){
-        if(keys.ArrowUp && player.y > road.top){player.y -= player.speed}
-        if(keys.ArrowDown && player.y < road.bottom){player.y += player.speed}
-        if(keys.ArrowLeft && player.x > 4){player.x -= player.speed}
-        if(keys.ArrowRight && player.x > road.width){player.x += player.speed}
+function keyDown(event) {}
 
-    spaceShip.top=player.y + 'px';
-    spaceShip.left=player.x + 'px';
-        
-    window.requestAnimationFrame(gamePlay);
-    score.innerText=" score: " + player.score;
-    player.score++;
-    }
-}
+function keyUp(event){}
 
-function start(){
-    startButton.classList.add('hide');
-
-    
-
-    player.start= true;
-    player.score= 0;
-
-    window.requestAnimationFrame(gamePlay);
-
-    
-    gameArea.appendChild(spaceShip);
-    player.x=spaceShip.offsetLeft;
-    player.y=spaceShip.offsetTop;
-    console.log(player.x);
-    console.log(player.y);
-
-}
+drawGame();
