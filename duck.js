9fa14d5bc483acda81080f502d657eec0d24ce7b
StinaAxelsson <stina91@live.se>
2021-08-04 13:50:15 +0000

    hej

--- duck.js
+++ duck.js
@@ -1,13 +1,27 @@
 const player = document.getElementById('player');
-const board = document.getElementsByClassName('board');
+const board = document.getElementById('gameboard');
 
-document.addEventListener('mousemove', function(e){
-    let x = e.clientX;
-    let y = e.clientY;
-    player.style.left = x + 'px';
-    player.style.top = y + 'px';
-    console.log('mousemove')
+let Key = {
+    ArrowLeft: 39,
+    ArrowRight: 37,
+}
 
-    if(e.clientY === )
 
+document.addEventListener('mousemove', handleMouseEvent);
+
+function handleMouseEvent(e) {
+    player.style.left = (e.pageX - player.width / 2) + 'px';
+}
+
+
+
+window.addEventListener("keydown", (e) => {
+    var left = parseInt(window.getComputedStyle(player).getPropertyValue("left"));
+    if (e.key == "ArrowLeft" && left > 0) {
+      player.style.left = left - 10 + "px";
+    }
+    //460  =>  board width - jet width
+    else if (e.key == "ArrowRight" && left <= 460) {
+      player.style.left = left + 10 + "px";
+    }
 });
